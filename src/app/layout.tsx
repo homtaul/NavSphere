3be0097f59806279ff712f1d71a8bd372ad3f73d
homtaul <homtaul@gmail.com>
2026-05-22 17:24:22 +0800

    Update layout.tsx
    1、SEO：增加谷歌分析

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,6 +62,19 @@ export default function RootLayout({
         )}
       </head>
       <body suppressHydrationWarning>
+        {/* Google Analytics 4 */}
+<Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-21DL43PWQG"
+  strategy="afterInteractive"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-21DL43PWQG');
+  `}
+</Script>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
